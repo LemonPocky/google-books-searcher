504111707d8b32e8682ed6b40bd8d14818c4b81e
LemonPocky <someone-70@live.com>
2021-08-22 14:32:57 -0700

    Added Apollo Client; Refactored /saved to use GraphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,16 @@
-// const { AuthenticationError } = require('apollo-server-express');
-// const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
 // const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {},
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
 
   Mutation: {},
 };
